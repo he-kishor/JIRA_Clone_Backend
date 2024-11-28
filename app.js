f7db79f1d6a8fb09a8d56edf8ad663b46488e7a0
hemuuolo <hemant.girase@uolo.com>
2024-11-28 23:28:28 +0530

    add cors middleware

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const Routes=require('./src/routes');
 const { logger } = require('./settings/middleware/auth_token');
 const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors');
 require('./src/Users/services/passport');
 
 const app = express();
 app.use(express.json());
 app.use(logger);
 
+
+
+
+
+app.use(cors()); // Enable CORS for all routes by default
+
+// Alternatively, enable CORS only for specific origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000', // Allow requests from this origin
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    })
+);
 
 //middleware
 // Session Middleware
